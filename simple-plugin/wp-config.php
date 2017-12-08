00093461bac622bff29f8179e6501a3b4a2fbe14
gauravprj <developerexpert002@gmail.com>
2017-12-08 15:30:53 +0530

    wp-config change

--- simple-plugin/wp-config.php
+++ simple-plugin/wp-config.php
@@ -23,7 +23,7 @@
 define('DB_NAME', 'simple-plugin_db');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'root');   
 
 /** MySQL database password */
 define('DB_PASSWORD', '');
